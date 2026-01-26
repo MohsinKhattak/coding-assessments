8c02484af4725f3af9d894a9eb4394d6b6dc2bc0
mohsinkhattak <khattakcodes@gmail.com>
2026-01-26 21:32:31 +0500

    Assesment: Find longest consecutive sub array

---
+++ longest_consecutive_sub_array.js
@@ -0,0 +1,27 @@
+function longest_sequence(nums) {
+  if (!nums || nums.length === 0) return 0;
+
+  const numSet = new Set(nums);
+  let longest = 0;
+
+  for (const num of numSet) {
+    if (!numSet.has(num - 1)) {
+      let current = num;
+      let length = 1;
+
+      while (numSet.has(current + 1)) {
+        current++;
+        length++;
+      }
+
+      longest = Math.max(longest, length);
+    }
+  }
+
+  return longest;
+}
+
+
+const list=[100, 4, 200, 1, 3, 2]
+
+console.log(longest_sequence(list))
